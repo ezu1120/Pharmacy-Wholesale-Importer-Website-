5568d07f61a4ae66cb71c4ddc26ffd020852700e
ezu1120 <laleezu1220@gmail.com>
2026-04-10 20:22:52 +0300

    feat: unique image per product using picsum.photos seed URLs

---
+++ backend/src/db/update-product-images.sql
@@ -0,0 +1,78 @@
+-- Assign a unique image to every product using picsum.photos (unique per ID number)
+-- Run in pgAdmin Query Tool on the pharmalink database
+
+UPDATE products SET image_url = 'https://picsum.photos/seed/amoxicillin/600/400'    WHERE name = 'Amoxicillin 500mg';
+UPDATE products SET image_url = 'https://picsum.photos/seed/atorvastatin/600/400'   WHERE name = 'Atorvastatin 20mg';
+UPDATE products SET image_url = 'https://picsum.photos/seed/metformin/600/400'      WHERE name = 'Metformin HCL 1000mg';
+UPDATE products SET image_url = 'https://picsum.photos/seed/lisinopril/600/400'     WHERE name = 'Lisinopril 10mg';
+UPDATE products SET image_url = 'https://picsum.photos/seed/lantus/600/400'         WHERE name = 'Lantus SoloStar';
+UPDATE products SET image_url = 'https://picsum.photos/seed/nexium/600/400'         WHERE name = 'Nexium 40mg';
+UPDATE products SET image_url = 'https://picsum.photos/seed/entresto/600/400'       WHERE name = 'Entresto 97/103mg';
+UPDATE products SET image_url = 'https://picsum.photos/seed/amlodipine/600/400'     WHERE name = 'Amlodipine 5mg';
+UPDATE products SET image_url = 'https://picsum.photos/seed/omeprazole/600/400'     WHERE name = 'Omeprazole 20mg';
+UPDATE products SET image_url = 'https://picsum.photos/seed/losartan/600/400'       WHERE name = 'Losartan 50mg';
+UPDATE products SET image_url = 'https://picsum.photos/seed/simvastatin/600/400'    WHERE name = 'Simvastatin 40mg';
+UPDATE products SET image_url = 'https://picsum.photos/seed/ciprofloxacin/600/400'  WHERE name = 'Ciprofloxacin 500mg';
+UPDATE products SET image_url = 'https://picsum.photos/seed/warfarin/600/400'       WHERE name = 'Warfarin 5mg';
+UPDATE products SET image_url = 'https://picsum.photos/seed/levothyroxine/600/400'  WHERE name = 'Levothyroxine 100mcg';
+UPDATE products SET image_url = 'https://picsum.photos/seed/gabapentin/600/400'     WHERE name = 'Gabapentin 300mg';
+
+UPDATE products SET image_url = 'https://picsum.photos/seed/aspirin/600/400'        WHERE name = 'Aspirin Protect 100mg';
+UPDATE products SET image_url = 'https://picsum.photos/seed/paracetamol/600/400'    WHERE name = 'Paracetamol 500mg';
+UPDATE products SET image_url = 'https://picsum.photos/seed/ibuprofen/600/400'      WHERE name = 'Ibuprofen 400mg';
+UPDATE products SET image_url = 'https://picsum.photos/seed/cetirizine/600/400'     WHERE name = 'Cetirizine 10mg';
+UPDATE products SET image_url = 'https://picsum.photos/seed/loratadine/600/400'     WHERE name = 'Loratadine 10mg';
+UPDATE products SET image_url = 'https://picsum.photos/seed/dextromethorphan/600/400' WHERE name = 'Dextromethorphan 15mg';
+UPDATE products SET image_url = 'https://picsum.photos/seed/antacid/600/400'        WHERE name = 'Antacid Tablets';
+UPDATE products SET image_url = 'https://picsum.photos/seed/loperamide/600/400'     WHERE name = 'Loperamide 2mg';
+UPDATE products SET image_url = 'https://picsum.photos/seed/dettol/600/400'         WHERE name = 'Dettol Antiseptic 500ml';
+UPDATE products SET image_url = 'https://picsum.photos/seed/zinccream/600/400'      WHERE name = 'Zinc Oxide Cream';
+UPDATE products SET image_url = 'https://picsum.photos/seed/ors/600/400'            WHERE name = 'Oral Rehydration Salts';
+
+UPDATE products SET image_url = 'https://picsum.photos/seed/n95mask/600/400'        WHERE name = 'N95 Respirator Mask';
+UPDATE products SET image_url = 'https://picsum.photos/seed/nitrilegloves/600/400'  WHERE name = 'Disposable Gloves M';
+UPDATE products SET image_url = 'https://picsum.photos/seed/ivset/600/400'          WHERE name = 'IV Infusion Set';
+UPDATE products SET image_url = 'https://picsum.photos/seed/gauzepad/600/400'       WHERE name = 'Sterile Gauze Pads 4x4';
+UPDATE products SET image_url = 'https://picsum.photos/seed/bandage/600/400'        WHERE name = 'Adhesive Bandages';
+UPDATE products SET image_url = 'https://picsum.photos/seed/syringe5ml/600/400'     WHERE name = 'Syringe 5ml';
+UPDATE products SET image_url = 'https://picsum.photos/seed/needle21g/600/400'      WHERE name = 'Hypodermic Needle 21G';
+UPDATE products SET image_url = 'https://picsum.photos/seed/wounddressing/600/400'  WHERE name = 'Wound Dressing 10x10cm';
+UPDATE products SET image_url = 'https://picsum.photos/seed/elasticbandage/600/400' WHERE name = 'Elastic Bandage 10cm';
+UPDATE products SET image_url = 'https://picsum.photos/seed/urinebag/600/400'       WHERE name = 'Urine Collection Bag';
+UPDATE products SET image_url = 'https://picsum.photos/seed/oxygenmask/600/400'     WHERE name = 'Oxygen Mask Adult';
+
+UPDATE products SET image_url = 'https://picsum.photos/seed/surggloves/600/400'     WHERE name = 'Surgical Gloves L';
+UPDATE products SET image_url = 'https://picsum.photos/seed/scalpel/600/400'        WHERE name = 'Scalpel Handle No.4';
+UPDATE products SET image_url = 'https://picsum.photos/seed/suture/600/400'         WHERE name = 'Surgical Suture 2-0';
+UPDATE products SET image_url = 'https://picsum.photos/seed/surgdrape/600/400'      WHERE name = 'Surgical Drape 150x200cm';
+UPDATE products SET image_url = 'https://picsum.photos/seed/esupencil/600/400'      WHERE name = 'Electrosurgical Pencil';
+UPDATE products SET image_url = 'https://picsum.photos/seed/stapler/600/400'        WHERE name = 'Surgical Stapler 30mm';
+UPDATE products SET image_url = 'https://picsum.photos/seed/retractor/600/400'      WHERE name = 'Retractor Set';
+UPDATE products SET image_url = 'https://picsum.photos/seed/surggown/600/400'       WHERE name = 'Surgical Gown XL';
+UPDATE products SET image_url = 'https://picsum.photos/seed/bonesaw/600/400'        WHERE name = 'Bone Saw Blade';
+UPDATE products SET image_url = 'https://picsum.photos/seed/trocar/600/400'         WHERE name = 'Laparoscopic Trocar 5mm';
+
+UPDATE products SET image_url = 'https://picsum.photos/seed/glucometer/600/400'     WHERE name = 'Blood Glucose Meter';
+UPDATE products SET image_url = 'https://picsum.photos/seed/pcrkit/600/400'         WHERE name = 'PCR Test Kit COVID-19';
+UPDATE products SET image_url = 'https://picsum.photos/seed/centrifuge/600/400'     WHERE name = 'Centrifuge Tubes 15ml';
+UPDATE products SET image_url = 'https://picsum.photos/seed/microscope/600/400'     WHERE name = 'Microscope Slides';
+UPDATE products SET image_url = 'https://picsum.photos/seed/dipstick/600/400'       WHERE name = 'Urine Dipstick 10 Panel';
+UPDATE products SET image_url = 'https://picsum.photos/seed/hbanalyzer/600/400'     WHERE name = 'Hemoglobin Analyzer';
+UPDATE products SET image_url = 'https://picsum.photos/seed/bloodculture/600/400'   WHERE name = 'Blood Culture Bottles';
+UPDATE products SET image_url = 'https://picsum.photos/seed/pipettetips/600/400'    WHERE name = 'Pipette Tips 200ul';
+UPDATE products SET image_url = 'https://picsum.photos/seed/malariardt/600/400'     WHERE name = 'Malaria RDT Kit';
+UPDATE products SET image_url = 'https://picsum.photos/seed/pregnancytest/600/400'  WHERE name = 'Pregnancy Test Strip';
+
+UPDATE products SET image_url = 'https://picsum.photos/seed/vitamind3/600/400'      WHERE name = 'Vitamin D3 1000IU';
+UPDATE products SET image_url = 'https://picsum.photos/seed/vitaminc/600/400'       WHERE name = 'Vitamin C 1000mg';
+UPDATE products SET image_url = 'https://picsum.photos/seed/omega3/600/400'         WHERE name = 'Omega-3 Fish Oil 1000mg';
+UPDATE products SET image_url = 'https://picsum.photos/seed/zinc50/600/400'         WHERE name = 'Zinc 50mg';
+UPDATE products SET image_url = 'https://picsum.photos/seed/magnesium/600/400'      WHERE name = 'Magnesium 400mg';
+UPDATE products SET image_url = 'https://picsum.photos/seed/probiotic/600/400'      WHERE name = 'Probiotic 10 Billion CFU';
+UPDATE products SET image_url = 'https://picsum.photos/seed/iron65/600/400'         WHERE name = 'Iron 65mg';
+UPDATE products SET image_url = 'https://picsum.photos/seed/folicacid/600/400'      WHERE name = 'Folic Acid 5mg';
+UPDATE products SET image_url = 'https://picsum.photos/seed/multivitamin/600/400'   WHERE name = 'Multivitamin Complete';
+UPDATE products SET image_url = 'https://picsum.photos/seed/calciumd3/600/400'      WHERE name = 'Calcium 600mg + D3';
+
+-- Verify: each product should have a unique image URL
+SELECT name, image_url FROM products ORDER BY category, name;
